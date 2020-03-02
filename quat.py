2f8da6409963385c5987d10a190419eb197de8a1
John Stechschulte <john.l.stechschulte@gmail.com>
2020-03-02 07:49:25 -0700

    more operations

--- quat.py
+++ quat.py
@@ -1,7 +1,10 @@
 class Quat:
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if len(args) == 0:
-            self.q = [0, 0, 0, 1]
+            if all(k in kwargs.keys() for k in "wxyz"):
+                self.q = [kwargs['x'], kwargs['y'], kwargs['z'], kwargs['w']]
+            elif len(kwargs)==0:
+                self.q = [0, 0, 0, 1]
         elif len(args) == 1:
             if len(args[0]) == 4 and all(map(lambda x: type(x) in (float, int), args[0])):
                 self.q = list(map(float, args[0]))
@@ -12,15 +15,48 @@ class Quat:
         else:
             raise ValueError("Invalid input--I can't make a quaternion from this")
 
+    def w(self): return self.q[3]
+    def x(self): return self.q[0]
+    def y(self): return self.q[1]
+    def z(self): return self.q[2]
+
     def __repr__(self):
         return "{} + {}i + {}j + {}k".format(self.q[3], self.q[0], self.q[1], self.q[2])
 
     def __str__(self):
         return self.__repr__()
 
+    def __neg__(self):
+        return Quat([-self.q[i] for i in range(4)])
+
     def __add__(self, other):
         if type(other) == Quat:
             return Quat([self.q[i] + other.q[i] for i in range(4)])
         else:
-            print(self.q[0:3] + [self.q[3] + other])
             return Quat(self.q[0:3] + [self.q[3] + other])
+
+    def __radd__(self, other):
+        return self + other
+
+    def __mul__(a, b):
+        if type(b) == Quat:
+            return Quat(
+                x = a.w()*b.x() + a.x()*b.w() + a.y()*b.z() - a.z()*b.y(),
+                y = a.w()*b.y() + a.y()*b.w() + a.z()*b.x() - a.x()*b.z(),
+                z = a.w()*b.z() + a.z()*b.w() + a.x()*b.y() - a.y()*b.x(),
+                w = a.w()*b.w() - a.x()*b.x() - a.y()*b.y() - a.z()*b.z(),
+                )
+        else:
+            return Quat([b*self.q[i] for i in range(4)])
+
+    def __sub__(self, other):
+        return self + -other
+
+    def __rsub__(self, other):
+        return -self + other
+
+    def __eq__(self, other):
+        return all(self.q[i] == other.q[i] for i in range(4))
+
+    def nearly_equal(self, other):
+        return all(abs(self.q[i] - other.q[i])<1e-6 for i in range(4))
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-import sys
+from context import quat
+Q = quat.Quat
 
-print(sys.path)
+q1 = Q([1, 2, 3, 4])
+q2 = Q(x=.1, y=.2, z=.3, w=.4)
 
-import quat
-
-q1 = quat.Quat([1, 2, 3, 4])
-q2 = quat.Quat([.1, .2, .3, .4])
-
-print(q1 + q2)
-print(q1 + 3)
-print(3 + q2)
+assert(q1 + q2 == Q([1.1, 2.2, 3.3, 4.4]))
+assert(q1 + 3 == Q([1, 2, 3, 7]))
+assert(3 + q2 == Q([.1, .2, .3, 3.4]))
+assert(q1 - q2 == Q([0.9, 1.8, 2.7, 3.6]))
+assert((q1*q2).nearly_equal(Q(w=0.2, x=0.8, y=1.6, z=2.4)))
